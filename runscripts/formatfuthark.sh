43c14501c47bbf27406f625dfc74336c11be9325
Lærke <laerke.b.pedersen@gmail.com>
2019-05-29 00:03:11 +0200

    cleanup

--- runscripts/formatfuthark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read line;\
-     do echo $line;\
-     if [[ $line == *"dataset"* ]];\
-          then numbers=$(echo $line | grep -Eo '[0-9]+([.][0-9]+)?' | tr '\n' ' ' | sed 's/^[ \t]*//;s/[ \t]*$//' | cut -d ' ' -f 2,3 | tr ' ' ';');\
-          echo $numbers >> $1;\
-     fi;\
-done
